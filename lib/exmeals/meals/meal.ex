84943be05578d44299f2fb0c655e1c76b4ea149f
rodrigo.vasconcelos <rodrigo.vasconcelos@siagri.com.br>
2021-06-09 08:57:25 -0300

    create changeset meals

---
+++ lib/exmeals/meals/meal.ex
@@ -0,0 +1,25 @@
+defmodule Exmeals.Meals.Meal do
+  use Ecto.Schema
+
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+
+  @required_params [:descricao, :data, :calorias]
+
+  schema "meals" do
+    field :desricao, :string
+    field :data, :naive_datetime
+    field :calorias, :integer
+
+    timestamps()
+  end
+
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> validate_length(:descricao, min: 6)
+    |> validate_number(:calorias, greater_than_or_equal_to: 0)
+  end
+end
